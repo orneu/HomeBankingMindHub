097def836873ff6d01600c4aeec09add002aa9d1
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-02-02 11:55:04 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                   new Client { Email = "ornellajazminpacino@gmail.com", FirstName = "Ornella", LastName = "Pacino", Password = "123456" }
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+
+    }
+}
---
+++ HomeBankingMindHub/Program.cs
@@ -0,0 +1,50 @@
+using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Add DbContext to the container.
+builder.Services.AddDbContext<HomeBankingContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
+
+
+var app = builder.Build();
+
+// Create a scope to get the DbContext instance
+
+using (var scope = app.Services.CreateScope())
+{
+
+    //Aqui obtenemos todos los services registrados en la App
+    var services = scope.ServiceProvider;
+    try
+    {
+
+        // En este paso buscamos un service que este con la clase HomeBankingContext
+        var context = services.GetRequiredService<HomeBankingContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ha ocurrido un error al enviar la información a la base de datos!");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+ 
+app.Run();
