4e231a22220834c58574a20eeb67c82860fa77a8
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-02-05 01:17:50 -0300

    Implemente los repositorios

---
+++ HomeBankingMindHub/Repositories/IClientRepository.cs
@@ -0,0 +1,11 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public interface IClientRepository
+    {
+        IEnumerable<Client> GetAllClients();
+        void Save(Client client);
+        Client FindById(long id);
+    }
+}
