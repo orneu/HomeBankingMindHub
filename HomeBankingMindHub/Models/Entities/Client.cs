015a5dd18a669a9a1d5169e6cac7add3f51f9c0d
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-02-08 17:24:01 -0300

    Creacion de la carpeta Entities

--- HomeBankingMindHub/Models/Entities/Client.cs
+++ HomeBankingMindHub/Models/Entities/Client.cs
@@ -8,6 +8,7 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public ICollection<Account> Accounts { get; set; }
-        public ICollection<ClientLoan> ClientLoans { get; set; }
+        public ICollection<ClientLoan> ClientLoan { get; set; }
+        public ICollection<Card> Cards { get; set; }
     }
 }
--- HomeBankingMindHub/Models/Entities/Transaction.cs
+++ HomeBankingMindHub/Models/Entities/Transaction.cs
@@ -1,11 +1,12 @@
 ﻿using HomeBankingMindHub.Models.DTOs;
+using HomeBankingMindHub.Models.ENUM;
 
 namespace HomeBankingMindHub.Models.Classes
 {
     public class Transaction
     {
         public long Id { get; set; }
-        public string Type { get; set; }
+        public TransactionType Type { get; set; }
         public double Amount { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
