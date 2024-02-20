7688f3e7b45e6ac9731da02fed56351881783739
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-02-20 16:00:51 -0300

    Data-Migration

---
+++ HomeBankingMindHub/Migrations/20240220061724_Transactions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeBankingMindHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class Transactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ClientId",
+                table: "Transactions",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ClientId",
+                table: "Transactions",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Clients_ClientId",
+                table: "Transactions",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Clients_ClientId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_ClientId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Transactions");
+        }
+    }
+}
