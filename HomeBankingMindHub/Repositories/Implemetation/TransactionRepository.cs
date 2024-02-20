9d4b1501fc3fbb1d916db703ee7d9272bc16d896
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-02-20 15:54:30 -0300

    Implementacion del Repositorio para Transactions

---
+++ HomeBankingMindHub/Repositories/Implemetation/TransactionRepository.cs
@@ -0,0 +1,30 @@
+﻿using HomeBankingMindHub.Models.Entities;
+using HomeBankingMindHub.Models;
+using HomeBankingMindHub.Repositories.Implemetation;
+using HomeBankingMindHub.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HomeBankingMindHub.Repositories.Implemetation
+{
+    public class TransactionRepository : RepositoryBase<Transaction>, ITransactionRepository
+    {
+        public TransactionRepository(HomeBankingContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public Transaction FindByNumber(long id)
+        {
+            return FindByCondition(transaction => transaction.Id == id).FirstOrDefault();
+        }
+
+        public void Save(Transaction transaction)
+        {
+            Create(transaction);
+            SaveChanges();
+        }
+    }
+}
+
---
+++ HomeBankingMindHub/Repositories/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using HomeBankingMindHub.Models.Entities;
+
+namespace HomeBankingMindHub.Repositories.Interfaces
+
+{
+    public interface ITransactionRepository
+    {
+        void Save(Transaction transaction);
+        Transaction FindByNumber(long id);
+    }
+}
